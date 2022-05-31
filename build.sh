bc0dba0626854153abfb52aff39c206361d974ff
Tyler Childerhose <tyler@tchilderhose.ca>
2022-05-31 06:59:45 -0400

    ignore-whitespace

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ git clone https://github.com/ProtonMail/proton-bridge.git
 cd proton-bridge
 git checkout $VERSION
 
-git apply ../patches/*.patch
+git apply --ignore-whitespace ../patches/*.patch
 
 # Build
 make build-nogui
