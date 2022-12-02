53345df8b6881c2fec317528662530e7a7ac5031
Tyler Childerhose <tyler@tchilderhose.ca>
2022-12-02 10:30:29 -0500

    ignore patches for now

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ git clone https://github.com/ProtonMail/proton-bridge.git
 cd proton-bridge
 git checkout $VERSION
 
-git apply --ignore-whitespace ../patches/*.patch
+#git apply --ignore-whitespace ../patches/*.patch
 
 # Build
 make build-nogui
